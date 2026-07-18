908a44ee0bc630fd0f08eefd601b03afe71993ce
kaire0145-art <kaire0145@gmail.com>
2026-07-18 18:42:07 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+// ===============================
+// HERO SLIDESHOW
+// ===============================
+
+const slides = document.querySelectorAll(".hero-slider img");
+
+let currentSlide = 0;
+
+function showNextSlide(){
+
+    slides[currentSlide].classList.remove("active");
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+
+        currentSlide = 0;
+
+    }
+
+    slides[currentSlide].classList.add("active");
+
+}
+
+setInterval(showNextSlide, 4000);
+
+// ===============================
+// STORY SLIDESHOW
+// ===============================
+
+const storySlides = document.querySelectorAll(".story-slider img");
+
+let currentStory = 0;
+
+function showNextStory(){
+
+    storySlides[currentStory].classList.remove("active-story");
+
+    currentStory++;
+
+    if(currentStory >= storySlides.length){
+
+        currentStory = 0;
+
+    }
+
+    storySlides[currentStory].classList.add("active-story");
+
+}
+
+setInterval(showNextStory, 4500);
