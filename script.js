e7d857e46fef84b5b34366bdbe63c2127ca70a0d
kaire0145-art <kaire0145@gmail.com>
2026-07-22 15:14:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -49,3 +49,9 @@ function showNextStory(){
 }
 
 setInterval(showNextStory, 4500);
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
